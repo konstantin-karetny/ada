dfb5732be6188d03029f4d542d50e15bcefeedc3
konstantin-karetny <konstantin.karetny@gmail.com>
2017-11-21 22:24:38 +0200

    .

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+    /**
+    * @package   ada/cms
+    * @version   1.0.0 02.10.2017
+    * @author    author
+    * @copyright copyright
+    * @license   Licensed under the Apache License, Version 2.0
+    */
+
+    $path_root = __DIR__;
+    require_once $path_root . '/scripts/vendor/ada/cms/index.php';
