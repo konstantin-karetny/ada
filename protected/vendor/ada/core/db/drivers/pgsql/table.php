5fcec5b4f702b006d00c025e457382b468c2401d
RK.120 <rk@maxx-marketing.net>
2018-03-28 18:06:29 +0300

    .

--- protected/vendor/ada/core/db/drivers/pgsql/table.php
+++ protected/vendor/ada/core/db/drivers/pgsql/table.php
@@ -35,31 +35,65 @@
                 return $res;
             }
             $db = $this->getDb();
-            var_dump( $db->fetchRows('
-                SELECT *
-                FROM '  . $db->q('information_schema') . '.' . $db->q('columns') . '
-                WHERE ' .
-                $db->q('table_name') . ' LIKE ' . $db->esc($this->getName(true))) );
 
+            echo                     '<pre>
+                    SELECT *
+                    FROM '  . $db->q('information_schema.columns') . ' AS ' . $db->q('c') . '
+                    LEFT JOIN '  . $db->q('information_schema.key_column_usage') . ' AS ' . $db->q('kcu') . '
+                    ON ' . $db->q('c.table_schema') . ' = ' . $db->q('kcu.table_schema') . '
+                    AND ' . $db->q('c.table_name') . ' = ' . $db->q('kcu.table_name') . '
+                    AND ' . $db->q('c.column_name') . ' = ' . $db->q('kcu.column_name') . '
+                    LEFT JOIN '  . $db->q('information_schema.table_constraints') . ' AS ' . $db->q('tc') . '
+                    ON ' . $db->q('c.table_schema') . ' = ' . $db->q('tc.table_schema') . '
+                    AND ' . $db->q('c.table_name') . ' = ' . $db->q('tc.table_name') . '
+                    AND ' . $db->q('kcu.constraint_name') . ' = ' . $db->q('tc.constraint_name') . '
+                    AND ' . $db->q('c.table_name') . ' LIKE ' . $db->esc($this->getName(true)) . '
+                    AND ' . $db->q('kcu.constraint_name') . ' IS NOT NULL
+                    AND ' . $db->q('tc.constraint_name') . ' IS NOT NULL
+                ';die;
 
-exit(var_dump(
-
-
-        $db->fetchRows('SELECT *
-FROM   pg_index i
-JOIN   pg_attribute a ON a.attrelid = i.indrelid
-                     AND a.attnum = ANY(i.indkey)
-WHERE  i.indrelid = \'' . $this->getName(true) . '\'::regclass
-AND    i.indisprimary;')
-
-
-
-
-
+            exit(var_dump(
 
 
 
-        ));
+                $db->fetchRows('
+                    SELECT *
+                    FROM '  . $db->q('information_schema.columns') . ' AS ' . $db->q('c') . '
+                    LEFT JOIN '  . $db->q('information_schema.key_column_usage') . ' AS ' . $db->q('kcu') . '
+                    ON ' . $db->q('c.table_schema') . ' = ' . $db->q('kcu.table_schema') . '
+                    AND ' . $db->q('c.table_name') . ' = ' . $db->q('kcu.table_name') . '
+                    AND ' . $db->q('c.column_name') . ' = ' . $db->q('kcu.column_name') . '
+                    LEFT JOIN '  . $db->q('information_schema.table_constraints') . ' AS ' . $db->q('tc') . '
+                    ON ' . $db->q('c.table_schema') . ' = ' . $db->q('tc.table_schema') . '
+                    AND ' . $db->q('c.table_name') . ' = ' . $db->q('tc.table_name') . '
+                    AND ' . $db->q('kcu.constraint_name') . ' = ' . $db->q('tc.constraint_name') . '
+                    AND ' . $db->q('c.table_name') . ' LIKE ' . $db->esc($this->getName(true)) . '
+                    AND ' . $db->q('kcu.constraint_name') . ' IS NOT NULL
+                    AND ' . $db->q('tc.constraint_name') . ' IS NOT NULL
+                ')
+
+            ));
+
+'select *
+from
+    information_schema.table_constraints tc,
+    information_schema.key_column_usage kc
+where
+    c.table_name LIKE ' . $db->esc($this->getName(true)) . '
+    AND c.column_name = kc.column_name
+    AND kc.table_name = tc.table_name
+    and kc.table_schema = tc.table_schema
+    and kc.constraint_name = tc.constraint_name';
+
+
+            exit(var_dump(
+                $db->fetchRows('
+                    SELECT *
+                    FROM '  . $db->q('information_schema') . '.' . $db->q('columns') . '
+                    WHERE ' .
+                    $db->q('table_name') . ' LIKE ' . $db->esc($this->getName(true))
+                )
+            ));
 
 
             foreach ($db->fetchRows('
